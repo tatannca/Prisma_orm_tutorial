d370c2fdd17c529804176cbf9dc8ad9d4dbbca33
tateno <gd28.c.tateno@gmail.com>
2021-07-25 18:21:16 +0900

    initial commit

---
+++ prisma/migrations/20210725084400_init/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE `Car` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `model` VARCHAR(191) NOT NULL,
+    `year` INTEGER NOT NULL,
+    `userId` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Car` ADD FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
